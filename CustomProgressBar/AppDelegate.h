f2ed670115a89829ef97ea5500365085ab881894
lordweimer91 <lordweimer91@gmail.com>
2018-07-26 13:30:50 +0600

    Initial Commit

---
+++ CustomProgressBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomProgressBar
+//
+//  Created by user on 26.07.18.
+//  Copyright © 2018 Sibers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomProgressBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomProgressBar
+//
+//  Created by user on 26.07.18.
+//  Copyright © 2018 Sibers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
